3f628b7046b6c803285efdce5c36327778a900df
imolep94 <79542627+imolep94@users.noreply.github.com>
2021-04-10 20:59:09 -0400

    Update functions.py
    Trader 10 a arteezy

--- functions.py
+++ functions.py
@@ -102,7 +102,7 @@ class main:
         #sentinela = True if ((me.id == 609697213) or (me.id == 705724375) or (me.id == 434324721) or (me.id == 887923662) or (me.id == 711652534)) else False
         sentinela = False
         tactics = "/tactics_eagles"
-        cod_trader = "09" if (me.id == 873541475) else ("55" if (me.id == 434324721) else "41")
+        cod_trader = "10" if (me.id == 711652534) else ("55" if (me.id == 434324721) else "41")
         trader = True if ((me.id == 609697213) or (me.id == 705724375) or (me.id == 434324721) or (me.id == 887923662) or (me.id == 711652534)) else False
         ofertas = True if ((me.id == 774368292) or (me.id == 716287267)) else False
         #knight = True if ((me.id == 835010542) or (me.id == 953357637) or (mainIds ["crazzy"]) or (mainIds ["sir"]) or (mainIds ["ines"]) or (mainIds ["barbaro"]) or (mainIds ["mad"]) or (mainIds ["imanol"]) or (cousinIds ["pumpkin"]) or (cousinIds ["niko"])) else False
